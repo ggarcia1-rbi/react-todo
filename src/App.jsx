e03fbef7ed51b0c1ae26b8c4d6856206b8ed2022
Oguzhan Dogan <oguzhandogan@Ozzys-MacBook-Pro.local>
2024-08-14 09:06:22 -0400

    async

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,54 @@ import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function useSemiPersistentState() {
-
-  const savedTodoList = JSON.parse(localStorage.getItem('savedTodoList')) || [];
-  const [todoList, setTodoList] = useState(savedTodoList);
-
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  }, [todoList]);
-
-  return [todoList, setTodoList];
-}
-
 function App() {
 
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const addTodo = (newTodo) =>
-    setTodoList([...todoList, newTodo]);
-  
+  useEffect(() => {
+    const fetchData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [] } });
+      }, 2000);
+    });
+
+    fetchData.then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
+
+  const addTodo = (newTodo) => setTodoList([...todoList, newTodo]);
+
   const removeTodo = (id) => {
     const updatedTodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(updatedTodoList);
-  }
+  };
 
-  return (
-    <>
-      <h1>Todo List</h1>
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
-      <AddTodoForm onAddTodo={addTodo} />
-    </>
-  );
+  if (isLoading) {
+    return (
+      <>
+        <h1>Todo List</h1>
+        <p>Loading...</p>
+        <AddTodoForm onAddTodo={addTodo} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Todo List</h1>
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+        <AddTodoForm onAddTodo={addTodo} />
+      </>
+    );
+  }
 }
 
 export default App;
